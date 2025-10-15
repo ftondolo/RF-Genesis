84f49830e01661a5b37171fd0674c086af0e7a65
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-10-15 02:04:18 -0400

    Update pathtracer.py

--- genesis/raytracing/pathtracer.py
+++ genesis/raytracing/pathtracer.py
@@ -153,10 +153,16 @@ def get_deafult_scene(res = 512):
 
 
 
-def trace(motion_filename):
-    smpl_data = np.load(motion_filename, allow_pickle=True)
-    root_translation = smpl_data['root_translation']
-    max_distance = np.max(root_translation[:,2])+2
+def trace(motion_filename=None):
+    if not motion_filename:
+        smpl_data = np.load(motion_filename, allow_pickle=True)
+        root_translation = smpl_data['root_translation']
+        max_distance = np.max(root_translation[:,2])+2
+        total_motion_frames = len(root_translation)
+        
+    total_motion_frames = 600
+    max_distance = 2
+    
     body_offset = np.array([0,1,3])
     sensor_origin = np.array([0,0,0])
     sensor_target = np.array([0,0,-5])
@@ -164,9 +170,8 @@ def trace(motion_filename):
     raytracer = RayTracer()
     PIRs = []
     pointclouds = []
-    total_motion_frames = len(root_translation)
 
-    for frame_idx in tqdm(range(0, total_motion_frames), desc="Rendering Body PIRs"):
+    for frame_idx in tqdm(range(0, total_motion_frames), desc="Rendering PLY PIRs"):
         rotation = mi.Transform4f.rotate([0,1,0], frame_idx * 1)
         raytracer.params_scene['smpl.to_world'] = rotation
         raytracer.params_scene.update()
