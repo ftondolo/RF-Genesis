cfccb20d3b53b5faa30dbcb5c7cd3461d864730d
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-10-27 05:08:57 -0400

    Comment out object_diff import in test.py
    Comment out the import statement for object_diff.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from genesis.raytracing import pathtracer
 from genesis.raytracing import signal_generator
 
 from genesis.environment_diffusion import environemnt_diff
-from genesis.object_diffusion import object_diff
+#from genesis.object_diffusion import object_diff
 from genesis.visualization import visualize
 
 
