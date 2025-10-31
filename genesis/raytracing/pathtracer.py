7af193e8b7c70c2b6490358ac7fb44a1ffc3f719
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-10-31 02:46:27 -0400

    Clean up unused code in pathtracer.py
    Removed commented-out code related to motion data and sensor setup.

--- genesis/raytracing/pathtracer.py
+++ genesis/raytracing/pathtracer.py
@@ -316,12 +316,6 @@ def trace(motion_filename=None, rotation_axis=[0,0,1], angle=3.6):
         pointclouds: List of pointcloud tensors (for radar pipeline)
         depth_pointclouds: List of depth pointclouds (for visualization)
     """
-    #smpl_data = np.load(motion_filename, allow_pickle=True)
-    #root_translation = smpl_data['root_translation']
-    #max_distance = np.max(root_translation[:,2])+2
-    #body_offset = np.array([0,1,3])
-    sensor_origin = np.array([0,0,0])
-    sensor_target = np.array([0,0,-5])
 
     raytracer = RayTracer()
     PIRs = []
