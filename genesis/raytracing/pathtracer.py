01a3a97a9f929e9dd9f54d81ff1b27cf74bb3bb4
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-11-07 09:10:49 -0500

    Update pathtracer.py

--- genesis/raytracing/pathtracer.py
+++ genesis/raytracing/pathtracer.py
@@ -278,7 +278,7 @@ def get_deafult_scene(res):
             'smpl':{
                 'type': 'ply',
                 'filename': '/content/RF-Genesis/models/trihedral.ply',
-                'to_world' : T.scale(1).translate([0, 0, 0]).rotate(axis=[0, 0, 1], angle=0),
+                'to_world' : T.scale(1).translate([0, 0, 0]).rotate(axis=[0, 0, 1], angle=45),
                 "mybsdf": {
                     "type": "ref",
                     "id": "while"
