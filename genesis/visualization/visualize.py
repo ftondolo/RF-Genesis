1167860cc594ed02f847744156f30cbe0013694d
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-11-13 17:17:05 -0500

    Update visualize.py

--- genesis/visualization/visualize.py
+++ genesis/visualization/visualize.py
@@ -18,12 +18,12 @@ def draw_depth_pointcloud(depth_pc, ax, elev, azim):
         return ax
     
     colors = depth_pc[:, 0]
-    ax.scatter(depth_pc[:, 0], -depth_pc[:, 2], depth_pc[:, 1], 
+    ax.scatter(depth_pc[:, 0], depth_pc[:, 1], depth_pc[:, 2], 
               c=colors, cmap=plt.cm.viridis, s=2, alpha=0.8)
     
     ax.set_xlabel('X')
-    ax.set_ylabel('Z')
-    ax.set_zlabel('Y')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
     ax.set_xlim(-0.25, 0.25)
     ax.set_ylim(0, 0.5)
     ax.set_zlim(-0.25, 0.25)
@@ -60,7 +60,7 @@ def draw_combined(i, pointcloud_cfg, radar_frames, radar_pointclouds, depth_poin
     fig = plt.figure(figsize=(12, 6))
 
     ax1 = fig.add_subplot(131, projection='3d')
-    draw_depth_pointcloud(depth_pointclouds[video_frame_id], ax1, elev=0, azim=180)
+    draw_depth_pointcloud(depth_pointclouds[video_frame_id], ax1, elev=0, azim=90)
 
     ax2 = fig.add_subplot(132, projection='3d')
     draw_poinclouds_on_axis(radar_pointclouds[i], ax2, None, None, 0, -90, "Radar Pointcloud")
