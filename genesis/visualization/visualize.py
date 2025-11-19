a60536603dc5077c28b3109edd8ebdc62330e033
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-11-18 20:32:27 -0500

    Update visualize.py

--- genesis/visualization/visualize.py
+++ genesis/visualization/visualize.py
@@ -60,7 +60,7 @@ def draw_combined(i, pointcloud_cfg, radar_frames, radar_pointclouds, depth_poin
     fig = plt.figure(figsize=(12, 6))
 
     ax1 = fig.add_subplot(131, projection='3d')
-    draw_depth_pointcloud(depth_pointclouds[video_frame_id], ax1, elev=0, azim=0)
+    draw_depth_pointcloud(depth_pointclouds[video_frame_id], ax1, elev=0, azim=90)
 
     ax2 = fig.add_subplot(132, projection='3d')
     draw_poinclouds_on_axis(radar_pointclouds[i], ax2, None, None, 0, 0, "Radar Pointcloud")
