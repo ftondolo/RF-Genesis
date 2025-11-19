91afc7f723e09aaa9a788963f7f66ed01363edca
Federico Tondolo <51007153+ftondolo@users.noreply.github.com>
2025-11-18 21:04:11 -0500

    Update visualize.py

--- genesis/visualization/visualize.py
+++ genesis/visualization/visualize.py
@@ -18,7 +18,7 @@ def draw_depth_pointcloud(depth_pc, ax, elev, azim):
         return ax
     
     colors = depth_pc[:, 0]
-    ax.scatter(-depth_pc[:, 0], depth_pc[:, 1], depth_pc[:, 2], 
+    ax.scatter(depth_pc[:, 0], depth_pc[:, 1], depth_pc[:, 2], 
               c=colors, cmap=plt.cm.viridis, s=2, alpha=0.8)
     
     ax.set_xlabel('X')
@@ -63,7 +63,7 @@ def draw_combined(i, pointcloud_cfg, radar_frames, radar_pointclouds, depth_poin
     draw_depth_pointcloud(depth_pointclouds[video_frame_id], ax1, elev=0, azim=90)
 
     ax2 = fig.add_subplot(132, projection='3d')
-    draw_poinclouds_on_axis(radar_pointclouds[i], ax2, None, None, 0, 0, "Radar Pointcloud")
+    draw_poinclouds_on_axis(radar_pointclouds[i], ax2, None, None, 0, 90, "Radar Pointcloud")
 
     ax3 = fig.add_subplot(133)
     draw_doppler_on_axis(radar_frames[i], pointcloud_cfg, ax3)
